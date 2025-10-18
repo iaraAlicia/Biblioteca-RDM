d5164dd4757324312861179184b6a11ce52c9eb8
Iara Alicia <iara44573@gmail.com>
2025-10-18 01:47:25 -0300

    estilização inicia

--- biblioteca_virtual/acervo/forms.py
+++ biblioteca_virtual/acervo/forms.py
@@ -7,20 +7,22 @@ class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
         fields = ['titulo', 'autor', 'editora', 'ano_publicacao', 'isbn']
-        # O campo 'disponivel' não está aqui porque por padrão, 
-        # um livro novo sempre estará disponível.
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'autor': forms.TextInput(attrs={'class': 'form-control'}),
+            'editora': forms.TextInput(attrs={'class': 'form-control'}),
+            'ano_publicacao': forms.TextInput(attrs={'class': 'form-control'}),
+            'isbn': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class EmprestimoForm(forms.ModelForm):
     class Meta:
         model = Emprestimo
         fields = ['livro', 'leitor', 'data_devolucao_prevista']
         widgets = {
-            'data_devolucao_prevista': forms.DateInput(attrs={'type': 'date'}),
-            
-            # Garantimos que os dois campos são do tipo Select
-            # e ambos têm a classe 'select2' para o JavaScript encontrar.
-            'livro': forms.Select(attrs={'class': 'select2'}),
-            'leitor': forms.Select(attrs={'class': 'select2'}),
+            'data_devolucao_prevista': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'livro': forms.Select(attrs={'class': 'form-control select2'}), # Adicionamos form-control
+            'leitor': forms.Select(attrs={'class': 'form-control select2'}), # Adicionamos form-control
         }
         
         
@@ -28,3 +30,9 @@ class LeitorForm(forms.ModelForm):
     class Meta:
         model = Leitor
         fields = ['nome', 'matricula', 'turma', 'telefone']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'matricula': forms.TextInput(attrs={'class': 'form-control'}),
+            'turma': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
